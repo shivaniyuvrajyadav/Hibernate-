629fbd864c93f06d3f4c41d7be118c8b54ff225b
shivaniyuvarajyadav <sy83801@gmail.com>
2023-07-19 12:00:46 -0700

    insert data using hibernate

---
+++ src/main/java/org/jsp/bike_wala/Bike.java
@@ -0,0 +1,61 @@
+package org.jsp.bike_wala;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Id;
+import javax.persistence.Persistence;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Bike {
+	@Id
+	private int id;
+	private String name;
+	private String color;
+	private double price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("dev");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction et = em.getTransaction();
+		
+		Bike b = new Bike ();
+		b.setId(102);
+		b.setName("Hero");
+		b.setColor("bike");
+		b.setPrice(85000);
+		
+		et.begin();
+		em.persist(b);
+		et.commit();
+	}
+	
+
+}
